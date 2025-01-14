57d9be2bfa00865afecaa6e7c1622a0053c96fcf
AlAminAshik <106368872+AlAminAshik@users.noreply.github.com>
2025-01-14 23:50:37 +0600

    removed display code

--- src/receive and view.cpp
+++ src/receive and view.cpp
@@ -1,5 +1,6 @@
 //ir sensor connected to PB1
-//generic remote: pwr=FFA25D; 0=FFB04F; 1=FF30CF; 2=FF18E7; 3=FF7A85; 4=FF10EF ; 5=FF38C7 ; 6=FF5AA5 ;
+//generic remote: pwr=FFA25D; 0=FFB04F; 1=FF30CF; 2=FF18E7; 3=FF7A85; 4=FF10EF ; 5=FF38C7 ; 6=FF5AA5 ;  //HEX
+//generic remote: pwr=-23971; 0=-20401; 1=12495; 2=6375; 3=31365; //DEC
 /*
  * Light Weight NEC Infrared decoder using interrupt instead of timer.
  *
@@ -8,27 +9,7 @@
  */
 
 #include "IrNec.h"
-#include <ss_oled.h>
-
-char mssg;
-
-//for oled
-// Use -1 for the Wire library default pins
-#define SDA_PIN -1
-#define SCL_PIN -1
-#define RESET_PIN -1
-#define OLED_ADDR -1
-// don't rotate the display
-#define FLIP180 0
-// don't invert the display
-#define INVERT 0
-// Bit-Bang the I2C bus
-#define USE_HW_I2C 0
-#define MY_OLED OLED_64x32
-#define OLED_WIDTH 64
-#define OLED_HEIGHT 32
-SSOLED ssoled;
-int rc;
+int16_t mssg;
 
 void setup() {
   Serial.begin(115200);
@@ -38,20 +19,9 @@ void setup() {
   // ESP8266 : Any GPIO pin apart from GPIO16
   // see attachInterrupt() for other examples and more details
   nsIrNec::begin(D5) ;  // MUST BE EXTERNAL INTERRUPT PIN 
-
-  //for oled
-  rc = oledInit(&ssoled, MY_OLED, OLED_ADDR, FLIP180, INVERT, USE_HW_I2C, SDA_PIN, SCL_PIN, RESET_PIN, 400000L); // use standard I2C bus at 400Khz
-  if (rc != OLED_NOT_FOUND)
-  {
-    char *msgs[] = {(char *)"SSD1306 @ 0x3C", (char *)"SSD1306 @ 0x3D",(char *)"SH1106 @ 0x3C",(char *)"SH1106 @ 0x3D",(char *)"SH1107 @ 0x3C",(char *)"SH1107 @ 0x3D"};
-    oledFill(&ssoled, 0, 1);
-    oledWriteString(&ssoled, 0,0,0,msgs[rc], FONT_NORMAL, 0, 1);
-    delay(2000);
-  }
 }
 
 void loop() {
-
   nsIrNec::loop() ;   // check for new data
 
   if ( nsIrNec::dataOut != 0 ) {
@@ -63,12 +33,9 @@ void loop() {
     // Serial.println();
 
     // print interpreted data then reset it
-    Serial.println(nsIrNec::dataOut,HEX) ;
-    Serial.println();
+    Serial.println(nsIrNec::dataOut,DEC) ;
+    Serial.println(mssg);
     mssg = nsIrNec::dataOut;
     nsIrNec::dataOut = 0 ; //clear
   }
-
-  oledFill(&ssoled, 0x0, 1);
-  oledWriteString(&ssoled, 0,16,0,(char *)mssg, FONT_NORMAL, 0, 0);
 }
