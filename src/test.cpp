3d699f2326df0a2716c443aad3b15c74724f3f4d
AlAminAshik <alaminashik890.bd@gmail.com>
2025-01-16 12:32:05 +0600

    optimized with display for wemos d1 and demonstration

--- src/test.cpp
+++ src/test.cpp
@@ -1,35 +1,34 @@
-//common code: =16729531; stop=16729786;
+//common code: pwr=16729531; stop=16729786;
+//common code: pwr=17843; stop=18098;
 #include <Arduino.h>
-const int irPin = PB4; // PB4 of attiny85 
-#define led 1    // PB0 of attiny85
+const int irPin = D5; // PB4 of attiny85 
+#define led D6    // PB0 of attiny85
 
 // I2C display connected at PB2(Pin 7 or SCL) and PB0(Pin 5 or SDA) of attiny85
 #include <Tiny4kOLED.h>
 
 void setup() {
-  //Serial.begin(115200);
+  Serial.begin(115200);
   pinMode(irPin, INPUT);
   pinMode(led, OUTPUT);
 
-  oled.begin(128, 64, sizeof(tiny4koled_init_128x64br), tiny4koled_init_128x64br);
+  oled.begin(128, 74, sizeof(tiny4koled_init_128x64br), tiny4koled_init_128x64br);
   oled.clear();
   oled.on();
   oled.setFont(FONT8X16P);  //set double font size
-  oled.print("IR Remote");
+  oled.print("OK");
   delay(2000);
   oled.clear();
 }
 
 int getIrKey(){
-  int len = pulseIn(irPin,LOW);
+  int len = pulseIn(irPin, LOW);
   int key, temp;
-  oled.clear();
   key = 0;
-  //Serial.print("len=");
   //Serial.println(len);
   if(len > 5000) {
-    for(int i=1;i<=32;i++){
-      temp = pulseIn(irPin,HIGH);
+    for(int i=1 ; i<=32 ; i++){
+      temp = pulseIn(irPin, HIGH);
       if(temp > 1000)
         key = key + (1<<(i-17));
     }
@@ -45,7 +44,14 @@ void loop() {
   int key = getIrKey();
   
   if(key != 0) {
-    oled.setCursor(10,10);
+    Serial.println(key);
+    oled.setCursor(20,10);
     oled.print(key);
+    if(key == 16729531){
+      digitalWrite(led, HIGH);
+    }
+    else if(key == 16729786){
+      digitalWrite(led, LOW);
+    }
   }
 }
