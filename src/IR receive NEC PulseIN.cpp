409b2526012276d0c09ee7e1e9b602112128abfc
AlAminAshik <alaminashik890.bd@gmail.com>
2025-01-21 15:57:45 +0600

    control light and fan using remote works!

--- src/IR receive NEC PulseIN.cpp
+++ src/IR receive NEC PulseIN.cpp
@@ -1,59 +1,62 @@
-//common code: pwr=16729531; stop=16729786;
-//common code: pwr=17843; stop=18098;
-#include <Arduino.h>
-#define irPin 1       // PB1 of attiny85
-#define led 3         // PB3 of attiny85
-uint32_t key;
+// //common code: pwr=16729531; stop=16729786; //esp
+// //common code: pwr=17843; stop=18098; //attiny85
+// //common code: 0=59678; 1=62228; 2=59168 ; 3=41318 ; 4=63248 ; 5=58148 ; 6=42338 ; 7=48458 ; 8=44378 ; 9=46418 ; -=63503 ; +=43102; //attiny85
 
-// I2C display connected at PB2(Pin 7 or SCL) and PB0(Pin 5 or SDA) of attiny85
-#include <Tiny4kOLED.h>
+// #include <Arduino.h>
+// #define irPin 1       // PB1 of attiny85
+// #define led 3         // PB3 of attiny85
+// uint16_t key;
 
-void setup() {
-  //Serial.begin(115200);
-  pinMode(irPin, INPUT);
-  pinMode(led, OUTPUT);
+// // I2C display connected at PB2(Pin 7 or SCL) and PB0(Pin 5 or SDA) of attiny85
+// #include <Tiny4kOLED.h>
 
-  oled.begin(128, 64, sizeof(tiny4koled_init_128x64br), tiny4koled_init_128x64br);
-  oled.setOffset(3, 0);
-  oled.clear();
-  oled.on();
-  oled.setFont(FONT8X16P);  //set double font size
-  oled.print("OK");
-  delay(2000);
-  oled.clear();
-}
+// void setup() {
+//   //Serial.begin(115200);
+//   pinMode(irPin, INPUT);
+//   pinMode(led, OUTPUT);
 
-uint32_t getIrKey(){
-  unsigned int len = pulseIn(irPin, LOW);
-  unsigned int temp = 0;
-  key = 0;
-  //Serial.println(len);
-  if(len > 5000) {
-    for(uint8_t i=1 ; i<=32 ; i++){
-      temp = pulseIn(irPin, HIGH);
-      if(temp > 1000)
-        key = key + (1<<(i-17));
-    }
-  }
-  if(key < 0 )
-    key = -key;
+//   oled.begin(128, 64, sizeof(tiny4koled_init_128x64br), tiny4koled_init_128x64br);
+//   oled.setOffset(3, 0);
+//   oled.clear();
+//   oled.on();
+//   oled.setFont(FONT8X16P);  //set double font size
+//   oled.print("OK");
+//   delay(2000);
+//   oled.clear();
+// }
 
-  delay(250);
-  return key;
-}
+// uint16_t getIrKey(){
+//   unsigned int len = pulseIn(irPin, LOW);
+//   unsigned int temp = 0;
+//   key = 0;
+//   //Serial.println(len);
+//   if(len > 5000) {
+//     for(uint8_t i=1 ; i<=32 ; i++){
+//       temp = pulseIn(irPin, HIGH);
+//       if(temp > 1000)
+//         key = key + (1<<(i-17));
+//     }
+//   }
+//   if(key < 0 )
+//     key = -key;
 
-void loop() {
-  getIrKey();
+//   delay(250);
+//   return key;
+// }
+
+// void loop() {
+//   getIrKey();
   
-  if(key != 0) {
-    //Serial.println(key);
-    oled.setCursor(20,10);
-    oled.print(key);
-    if(key == 16729531){
-      digitalWrite(led, HIGH);
-    }
-    else if(key == 16729786){
-      digitalWrite(led, LOW);
-    }
-  }
-}
+//   if(key != 0) {
+//     //Serial.println(key);
+//     oled.clear();
+//     oled.setCursor(20,10);
+//     oled.print(key);
+//     if(key == 167){
+//       digitalWrite(led, HIGH);
+//     }
+//     else if(key == 16729){
+//       digitalWrite(led, LOW);
+//     }
+//   }
+// }
---
+++ src/main.cpp
@@ -0,0 +1,65 @@
+//This code is for attiny13A
+//common code: pwr=17843; stop=18098;
+//common code: 0=59678; 1=62228; 2=59168 ; 3=41318 ; 4=63248 ; 5=58148 ; 6=42338 ; 7=48458 ; 8=44378 ; 9=46418 ; -=63503 ; +=43102;
+
+#include <Arduino.h>
+#define irPin 1         // PB1 of attiny85
+#define Fan 0           // PB0 of attiny85
+#define led_1 2         // PB2 of attiny85
+#define led_2 3         // PB3 of attiny85
+
+//uint16_t pot_values[6] = {59678, 62228, 59168, 41318, 63248, 58148};
+
+void setup() {
+    pinMode(irPin, INPUT);
+    pinMode(led_1, OUTPUT);
+    pinMode(led_2, OUTPUT);
+    pinMode(Fan, OUTPUT);
+}
+
+uint16_t getIrKey(){
+  unsigned int len = pulseIn(irPin, LOW);
+  unsigned int temp = 0;
+	uint16_t key = 0;
+  if(len > 5000) {
+    for(uint8_t i=1 ; i<=32 ; i++){
+      temp = pulseIn(irPin, HIGH);
+      if(temp > 1000)
+        key = key + (1<<(i-17));
+    }
+  }
+  if(key < 0 )
+    key = -key;
+
+  delay(250);
+  return key;
+}
+
+void loop() {
+  uint16_t key = getIrKey();
+
+	if (key != 0)
+	{
+		switch (key)
+		{
+		case 42338: //6
+			digitalWrite(led_1, HIGH);
+			break;
+		case 48458: //7
+			digitalWrite(led_1, LOW);
+			break;
+		case 44378: // 8
+			digitalWrite(led_2, HIGH);
+			break;
+		case 46418: // 9
+			digitalWrite(led_2, LOW);
+			break;
+		case 59678: // 0
+			digitalWrite(Fan, HIGH);
+			break;
+		case 62228: // 1
+			digitalWrite(Fan, LOW);
+			break;
+		}
+	}
+}
