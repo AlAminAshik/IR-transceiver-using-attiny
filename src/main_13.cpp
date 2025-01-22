a4868f0356c0b759ab4e3d75d6063425c0970ba1
AlAminAshik <106368872+AlAminAshik@users.noreply.github.com>
2025-01-22 22:56:55 +0600

    single button on/off

--- src/main_13.cpp
+++ src/main_13.cpp
@@ -6,26 +6,26 @@
 //common code: 0=59678; 1=62228; 2=59168 ; 3=41318 ; 4=63248 ; 5=58148 ; 6=42338 ; 7=48458 ; 8=44378 ; 9=46418 ; -=63503 ; +=43102;
 
 #include <Arduino.h>
-#define irPin 4         // PB1 of attiny85
+#define ir_PIN 4         // PB1 of attiny85
 #define ZC_PIN 1        // Zero Crossing Detection (ZCD) pin //has interrupt and PWM
-#define Fan 0           // PB0 of attiny85  //Has PWM
-#define led_1 2         // PB2 of attiny85
-#define led_2 3         // PB3 of attiny85
+#define Fan_PIN 0           // PB0 of attiny85  //Has PWM
+#define led_1_PIN 2         // PB2 of attiny85
+bool last_led_1_state;
+#define led_2_PIN 3         // PB3 of attiny85
+bool last_led_2_state;
 
 // Speed levels (lower values = faster speed)
-const int speedLevels[5] = {180, 130, 90, 50, 10};  // Phase delay values
+const int speedLevels[5] = {180, 130, 90, 50, 10};  // Phase delay values //180 means off
 
 volatile int speedIndex = 0;  // Current speed level index (0 to 4)
 volatile int firingDelay = speedLevels[speedIndex];
 
 void zeroCrossISR() {
     delayMicroseconds(firingDelay);
-    digitalWrite(Fan, HIGH);
+    digitalWrite(Fan_PIN, HIGH);
     delayMicroseconds(10);  // Short pulse to trigger TRIAC
-    digitalWrite(Fan, LOW);
+    digitalWrite(Fan_PIN, LOW);
 }
-
-//uint16_t pot_values[6] = {59678, 62228, 59168, 41318, 63248, 58148};
 
 
 void setup() {
@@ -34,19 +34,20 @@ void setup() {
     CLKPR = 0b0000;  // Set prescaler to 1 (No division, full speed)
     sei();  // Enable interrupts
 
-    pinMode(irPin, INPUT);
-    pinMode(led_1, OUTPUT);
-    pinMode(led_2, OUTPUT);
-    pinMode(Fan, OUTPUT);
+    pinMode(ir_PIN, INPUT_PULLUP);
+    pinMode(led_1_PIN, OUTPUT);
+    pinMode(led_2_PIN, OUTPUT);
+    pinMode(Fan_PIN, OUTPUT);
+    attachInterrupt(digitalPinToInterrupt(ZC_PIN), zeroCrossISR, RISING);
 }
 
 uint16_t getIrKey(){
-  unsigned int len = pulseIn(irPin, LOW);
+  unsigned int len = pulseIn(ir_PIN, LOW);
   unsigned int temp = 0;
 	uint16_t key = 0;
   if(len > 2500) {                      //first high bit
     for(uint8_t i=1 ; i<=32 ; i++){
-      temp = pulseIn(irPin, HIGH);
+      temp = pulseIn(ir_PIN, HIGH);
       if(temp > 500)                    //proceeding low bits
         key = key + (1<<(i-17));
     }
@@ -63,30 +64,32 @@ void loop() {
 
 	if (key != 0)
 	{
-		digitalWrite(led_1, HIGH);
-		delay(100);
-		digitalWrite(led_1, LOW);
-		delay(100);
 		switch (key)
-		{
-		case 42338: //6
-			digitalWrite(led_1, HIGH);
-			break;
-		case 48458: //7
-			digitalWrite(led_1, LOW);
-			break;
-		case 44378: // 8
-			digitalWrite(led_2, HIGH);
-			break;
-		case 46418: // 9
-			digitalWrite(led_2, LOW);
-			break;
-		case 59678: // 0
-			digitalWrite(Fan, HIGH);
-			break;
-		case 62228: // 1
-			digitalWrite(Fan, LOW);
-			break;
-		}
-	}
+    {
+    case 58148: // 5
+            digitalWrite(led_1_PIN, last_led_1_state);
+            last_led_1_state = !last_led_1_state;
+        break;
+    case 42338: // 6
+            digitalWrite(led_2_PIN, last_led_2_state);
+            last_led_2_state = !last_led_2_state;
+        break;
+    case 59678: // 0
+        speedIndex = 0;
+        break;
+    case 62228: // 1
+        speedIndex = 1;
+        break;
+    case 59168: // 2
+        speedIndex = 2;
+        break;
+    case 41318: // 3
+        speedIndex = 3;
+        break;
+    case 63248: // 4
+        speedIndex = 4;
+        break;
+    }
+    firingDelay = speedLevels[speedIndex]; //update speed
+    }
 }
