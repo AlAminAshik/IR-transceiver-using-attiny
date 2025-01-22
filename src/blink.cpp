bc70eb70fcf178d1ed09f6033e772e63059ceb9b
AlAminAshik <alaminashik890.bd@gmail.com>
2025-01-22 11:02:30 +0600

    debug blink code added

---
+++ src/blink.cpp
@@ -0,0 +1,13 @@
+// #include <Arduino.h>
+// #define led_1 2         // PB2 of attiny85
+
+// void setup(){
+//     pinMode(led_1, OUTPUT);
+// }
+
+// void loop(){
+//     digitalWrite(led_1, HIGH);
+//     delay(1000);
+//     digitalWrite(led_1, LOW);
+//     delay(1000);
+// }
