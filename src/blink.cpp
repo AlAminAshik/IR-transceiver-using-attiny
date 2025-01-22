9797d0ac7c51056c0d0312689e7deae4b1801dbb
AlAminAshik <106368872+AlAminAshik@users.noreply.github.com>
2025-01-22 20:35:12 +0600

    attiny13 code finally works!

--- src/blink.cpp
+++ src/blink.cpp
@@ -1,20 +1,25 @@
-#include <Arduino.h>
-#define led_1 2         // PB2 of attiny85
-#define led_2 3         // PB3 of attiny85
+// #include <Arduino.h>
+// #define led_1 2         // PB2 of attiny85
+// #define led_2 3         // PB3 of attiny85
 
 
-void setup(){   
-    pinMode(led_1, OUTPUT);
-    pinMode(led_2, OUTPUT);
-}
+// void setup(){   
+//     cli();  // Disable interrupts
+//     CLKPR = (1 << CLKPCE);  // Enable Clock Prescaler Change
+//     CLKPR = 0b0000;  // Set prescaler to 1 (No division, full speed)
+//     sei();  // Enable interrupts
 
-void loop(){
-    digitalWrite(led_1, HIGH);
-    delay(1000);
-    digitalWrite(led_1, LOW);
-    delay(1000);
-    digitalWrite(led_2, HIGH);
-    delay(1000);
-    digitalWrite(led_2, LOW);
-    delay(1000);
-}
+//     pinMode(led_1, OUTPUT);
+//     pinMode(led_2, OUTPUT);
+// }
+
+// void loop(){
+//     digitalWrite(led_1, HIGH);
+//     delay(1000);
+//     digitalWrite(led_1, LOW);
+//     delay(1000);
+//     digitalWrite(led_2, HIGH);
+//     delay(1000);
+//     digitalWrite(led_2, LOW);
+//     delay(1000);
+// }
